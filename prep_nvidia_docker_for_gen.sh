c90d06b7a5b146f74f941646f916c40b2bdc999e
velilemaqolombeni <162371299+velilemaqolombeni@users.noreply.github.com>
2024-03-06 22:44:24 +0200

    Create prep_nvidia_docker_for_gen.sh

---
+++ prep_nvidia_docker_for_gen.sh
@@ -0,0 +1,22 @@
+apt update
+apt -y purge docker.io;apt -y autoremove docker.io
+apt -y install docker.io
+systemctl start docker
+systemctl enable docker
+docker --version
+
+usermod -a -G docker $USER
+newgrp docker
+
+
+apt -y install curl
+
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | tee /etc/apt/sources.list.d/nvidia-docker.list
+
+apt update
+apt -y install nvidia-container-toolkit
+systemctl restart docker
+
+docker run --gpus all nvidia/cuda:10.0-base nvidia-smi
